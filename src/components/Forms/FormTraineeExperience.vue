408cfff57c642a1f5c05611a2ad903c2877bdc20
ShengzheWang <1005709383@qq.com>
2018-04-12 08:20:59 +0800

    修改表单绑定v-model
    修改简历部分的9张表单

--- src/components/Forms/FormTraineeExperience.vue
+++ src/components/Forms/FormTraineeExperience.vue
@@ -3,22 +3,21 @@
     <div style="width:90%;margin: 0% auto;">
       <div style="width:100%;height:30px">
       </div>
-      <el-form label-position="labelPosition" label-width="140px">
+      <el-form label-position="labelPosition" label-width="140px" >
         <el-form-item label="起始日期">
-          <el-date-picker type="date" placeholder="选择日期"
+          <el-date-picker type="date" placeholder="选择日期" v-model="formTrainee.trainee_start_time"
                           class="input-date"></el-date-picker>
         </el-form-item>
         <el-form-item label="结束日期">
-          <el-date-picker type="date" placeholder="选择日期"
+          <el-date-picker type="date" placeholder="选择日期" v-model="formTrainee.trainee_end_time"
                           class="input-date"></el-date-picker>
         </el-form-item>
         <el-form-item label="实习公司" style="width: 50%">
-          <el-input></el-input>
+          <el-input v-model="formTrainee.trainee_company"></el-input>
         </el-form-item>
         <el-form-item label="实习职位" style="width: 50%">
-          <el-input></el-input>
+          <el-input v-model="formTrainee.trainee_position"></el-input>
         </el-form-item>
-
         <el-form-item label="还有其他实习经历？" style="width: 50%">
           <el-button><i class="el-icon-plus"></i></el-button>
         </el-form-item>
@@ -32,19 +31,26 @@
   </div>
 </template>
 <script>
-export default {
-  data () {
-    return {
-      select: '',
-      imageUrl: ''
-    }
-  },
-  methods: {
-    nextStep () {
-      this.$router.push('/ResumeForm/7')
+  export default {
+    data () {
+      return {
+        formTrainee:{
+          ID:null,
+          resume_id:null,
+          trainee_start_time:'',
+          trainee_end_time:'',
+          trainee_company:'',
+          trainee_position:''
+        }
+      }
+    },
+    methods: {
+      nextStep () {
+        this.$router.push('/ResumeForm/7')
+      },
+
     }
   }
-}
 </script>
 <style scoped>
   .input-date{
