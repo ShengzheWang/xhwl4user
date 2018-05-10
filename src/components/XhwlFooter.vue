f166fad345a308ca02aebbe96305b70a95412afa
imlixinyang <1034298388@qq.com>
2018-05-10 16:21:03 +0800

    压缩图片

--- src/components/XhwlFooter.vue
+++ src/components/XhwlFooter.vue
@@ -3,12 +3,12 @@
     <div class="footer">
       <div class="wrap">
         <ul class="friend-link">
-          <li><a href="//www.jd.com" target="_blank">兴海物联
+          <li><a  target="_blank">兴海物联
             官方网站</a></li>
           <li>|</li>
-          <li><a href="//zhaopin.jd.com" target="_blank">中建集团</a></li>
+          <li><a target="_blank">中建集团</a></li>
           <li>|</li>
-          <li><a href="//xiaoyuan.jd.com" target="_blank">校园团队</a></li>
+          <li><a target="_blank">校园团队</a></li>
         </ul>
       </div>
       <div style="text-align: center;width: 100%">
