8501b93a4d8d18199135b189ace2b49fc1d14bd0
imlixinyang <1034298388@qq.com>
2018-05-14 12:36:20 +0800

    更新bug

--- src/components/mineRouters/MyResume.vue
+++ src/components/mineRouters/MyResume.vue
@@ -1,7 +1,6 @@
 <template>
   <div ref="c" id="MyResume" style="height: 700px;width: 1300px;margin:0% auto;" v-loading="loading">
     <h1 v-bind:class="'animated fade'+show2" style="animation-delay:0.6s;width: 1100px;margin:5% auto"> 你还未创建过简历，请单击你想创建的简历类型开始填写简历</h1>
-    <div style="height: 350px;width: 1100px;margin:5% auto">
       <el-tooltip class="item" effect="dark" content="创建校园招聘简历" placement="top-end" >
       <div @click="createAResume(1)" v-bind:class="'animated bounce'+show" id="circle1"
            v-bind:style="'animation-delay:'+time[0]+'s'">
@@ -82,7 +81,7 @@ export default {
         url: '/resume?form=' + whichOne
       }).then(function (response) {
         setTimeout(() => {
-          _this.$router.push({ name: 'ResumeForm', params: {resumes_form_selected: whichOne.toString()} })
+          _this.$router.push({ name: 'ResumeForm', query: {resumes_form_selected: whichOne.toString()} })
         }, 1000)
       })
     },
